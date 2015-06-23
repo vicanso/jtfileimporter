cbd4fffab681e04947befd88e40bc616c0d19761
vicanso <vicansocanbico@gmail.com>
2015-06-23 22:26:52 +0800

    es6

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,38 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 var cover = require('gulp-coverage');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('jshint', function() {
   return gulp.src('./lib/*.js')
     .pipe(jshint({
       predef : ['require', 'module'],
-      node : true
+      node : true,
+      esnext : true
     }))
     .pipe(jshint.reporter('default'));
 });
 
 gulp.task('test', function(){
   return gulp.src('./test/*.js')
-    .pipe(cover.instrument({
-      pattern : ['./lib/*.js']
-    }))
+    .pipe(istanbul())
     .pipe(mocha({reporter: 'spec'}))
-    .pipe(cover.report({
-      outFile : 'coverage.html'
+    .pipe(istanbul.writeReports())
+    .pipe(istanbul.enforceThresholds({
+      thresholds : {
+        global : 90
+      }
     }));
 });
 
 gulp.task('default', ['jshint', 'test']);
+
+
+
+// .on('finish', function () {
+//       gulp.src(['test/*.js'])
+//         .pipe(mocha())
+//         .pipe(istanbul.writeReports()) // Creating the reports after tests ran
+//         .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } })) // Enforce a coverage of at least 90%
+//         .on('end', cb);
+//     });
